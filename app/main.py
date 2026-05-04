ad521d6ec3157b7be095a2c3db800c43bb3c096e
midnightGlow <119887004+AnusreeRoy@users.noreply.github.com>
2026-05-04 17:18:58 +0600

    update

--- app/main.py
+++ app/main.py
@@ -13,6 +13,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
+        allow_origins=[
+        "http://localhost:3000",
+        "https://your-vercel-app.vercel.app"
+    ],
     allow_origins=["*"],  # for dev (restrict later)
     allow_credentials=True,
     allow_methods=["*"],
